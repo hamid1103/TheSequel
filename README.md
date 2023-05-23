029173814a7cf4dc1116ed05894311e557cb0df8
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-05-23 18:15:58 +0200

    setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# TheSequel
-The Sequel is a very Serious Game about a guy trying to find his hacker friend, by hacking into databases with sql queries
---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    
+  </div>
+`
