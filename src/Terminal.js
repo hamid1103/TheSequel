bb4c4d6711e53c983cf22ebdf9813cff5be3cbdc
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-06-09 15:48:09 +0200

    SQL interface prototype

--- src/Terminal.js
+++ src/Terminal.js
@@ -2,15 +2,14 @@ import {variants} from "@catppuccin/palette";
 import {engine} from "../main.js";
 
 export class Terminal {
-    _engine
     MainDiv = document.getElementById('Main');
     terminal = document.createElement('div')
     sidebar = document.createElement('div')
     topbarInstance = new Topbar()
     topbar = this.topbarInstance.topbar
     termwin = document.createElement('div')
-    browserinst = new BrowserWindow()
-    browserdiv = this.browserinst.browserwindow
+    browserinst
+    browserdiv
 
     constructor(_engine) {
         this._engine = _engine
@@ -19,6 +18,8 @@ export class Terminal {
         //ID's shouls help with making the css
         this.MainDiv.classList = 'center'
 
+        this.browserinst  = new BrowserWindow(this._engine)
+        this.browserdiv = this.browserinst.browserwindow
         this.browserdiv.id = 'BrowserWindow'
         this.terminal.id = 'Terminal'
         this.sidebar.id = 'Sidebar'
@@ -43,15 +44,17 @@ export class Terminal {
         this.terminal.appendChild(this.termwin)
         this.MainDiv.appendChild(this.terminal)
     }
-    Update(){
+
+    Update() {
     }
 }
 
-export class Topbar{
+export class Topbar {
     topbar;
     tabs;
     currentActiveTab;
     alertTab;
+
     constructor() {
         this.topbar = document.createElement('div')
     }
@@ -60,7 +63,42 @@ export class Topbar{
 export class BrowserWindow {
     browserwindow
     currentTab;
-    constructor() {
+    currentQuerry = "select * from Main where id=1";
+
+    constructor(_engine ,currentTab = 'DBQ') {
         this.browserwindow = document.createElement('div')
+        this.currentTab = currentTab;
+        this._engine = _engine
+        this.onInit();
     }
+
+    callQuerry =(e)=>{
+        let QRes = this._engine.db.exec(e.target.value)
+        this.result = QRes[0]
+        this.DBOutput.innerText = JSON.stringify(this.result)
+        console.log(this.result)
+    }
+
+    onInit() {
+        if (this.currentTab === 'DBQ') {
+            this.DBQ = document.createElement('div')
+            this.DBQ.id = 'DBQ'
+            this.label = document.createElement('label')
+            this.label.innerText = "SQL Query"
+            this.DBinput = document.createElement('input')
+            this.DBinput.id = 'DBInput'
+            this.DBinput.value = this.currentQuerry
+            this.DBOutput = document.createElement('textarea')
+            this.DBOutput.disabled = true
+            this.DBOutput.id="DBOutput"
+
+            this.DBinput.addEventListener('change', this.callQuerry)
+            this.DBQ.appendChild(this.label)
+            this.DBQ.appendChild(this.DBinput)
+            this.DBQ.appendChild(this.DBOutput)
+            this.browserwindow.appendChild(this.DBQ);
+        }
+
+    }
+
 }
